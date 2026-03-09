6b19dd9f920cdc826133fd05f91174a5ee8ff9c8
alexandrebrito174DEV <alexandrebrito174@gmail.com>
2026-03-09 10:00:20 +0000

    API v1.0.16 - melhorias nos comentários e organização

--- api/server.js
+++ api/server.js
@@ -1,3 +1,6 @@
+// Configurações iniciais
+
+// Ficheiro .env
 require("dotenv").config();
 
 const { PrismaClient } = require("@prisma/client");
@@ -6,6 +9,7 @@ const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
 
+// Ligação à base de dados
 const adapter = new PrismaPg({
   connectionString: process.env.DATABASE_URL
 });
@@ -20,14 +24,13 @@ app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 
+// Porta
 const PORT = process.env.SERVER_PORT || 4242;
 
-// --------------------------------------------------------------------
-// Rotas base
-// --------------------------------------------------------------------
+// Rotas Gerais
 
 app.get("/", (req, res) => {
-  return res.status(200).json({ message: "Hello, my API works 🚀 v1.0.11" });
+  return res.status(200).json({ message: "Hello, my API works 🚀 v1.0.17" });
 });
 
 app.get("/users", (req, res) => {
@@ -46,10 +49,12 @@ app.delete("/users/:id", (req, res) => {
   return res.status(200).json({ message: "OK - DELETE users" });
 });
 
+
 // --------------------------------------------------------------------
 // API de filmes
 // --------------------------------------------------------------------
 
+// Array - filmes
 let filmes = [
   { id: 1, titulo: "Inception", ano: 2010 },
   { id: 2, titulo: "Interstellar", ano: 2014 }
@@ -62,15 +67,13 @@ app.get("/filmes", (req, res) => {
 app.get("/filmes/:id", (req, res) => {
   const id = parseInt(req.params.id);
 
-  if (isNaN(id))
-  {
+  if (isNaN(id)){
     return res.status(400).json({ erro: "Id inválido" });
   }
 
   const filme = filmes.find((filmeAtual) => filmeAtual.id === id);
 
-  if (!filme)
-  {
+  if (!filme){
     return res.status(404).json({ erro: "Filme não encontrado" });
   }
 
@@ -81,8 +84,7 @@ app.post("/filmes", (req, res) => {
   const titulo = req.body.titulo;
   const ano = req.body.ano;
 
-  if (!titulo || !ano)
-  {
+  if (!titulo || !ano){
     return res.status(400).json({ erro: "Titulo e ano são obrigatórios" });
   }
 
@@ -100,28 +102,24 @@ app.post("/filmes", (req, res) => {
 app.put("/filmes/:id", (req, res) => {
   const id = parseInt(req.params.id);
 
-  if (isNaN(id))
-  {
+  if (isNaN(id)){
     return res.status(400).json({ erro: "Id inválido" });
   }
 
   const filme = filmes.find((filmeAtual) => filmeAtual.id === id);
 
-  if (!filme)
-  {
+  if (!filme){
     return res.status(404).json({ erro: "Filme não encontrado" });
   }
 
   const titulo = req.body.titulo;
   const ano = req.body.ano;
 
-  if (titulo)
-  {
+  if (titulo){
     filme.titulo = titulo;
   }
 
-  if (ano)
-  {
+  if (ano){
     filme.ano = ano;
   }
 
@@ -131,40 +129,49 @@ app.put("/filmes/:id", (req, res) => {
 app.delete("/filmes/:id", (req, res) => {
   const id = parseInt(req.params.id);
 
-  if (isNaN(id))
-  {
+  if (isNaN(id)){
     return res.status(400).json({ erro: "Id inválido" });
   }
 
   const indice = filmes.findIndex((filmeAtual) => filmeAtual.id === id);
 
-  if (indice === -1)
-  {
+  if (indice === -1){
     return res.status(404).json({ erro: "Filme não encontrado" });
   }
 
-  filmes.splice(indice, 1);
+  
+  
+  filmes.splice(indice, 1); // splice - remove elementos do array (a partir da posição encontrada)
 
   return res.status(200).json({ mensagem: "Filme removido" });
 });
+
 
 // --------------------------------------------------------------------
 // API de tarefas
 // --------------------------------------------------------------------
 
+// Prioridades
 const PRIORIDADES = ["baixa", "média", "alta"];
 
-function prioridadeValida(prioridade)
-{
-  return PRIORIDADES.includes(prioridade);
+// Verifica se a prioridade inserida existe
+function prioridadeValida(prioridade){
+  let i;
+
+  for (i = 0; i < PRIORIDADES.length; i++){
+    if (PRIORIDADES[i] === prioridade){
+      return true;
+    }
+  }
+
+  return false;
 }
 
-function parseIdTarefa(param)
-{
+// Converte o id recebido para número
+function parseIdTarefa(param){
   const id_tarefa = parseInt(param);
 
-  if (isNaN(id_tarefa))
-  {
+  if (isNaN(id_tarefa)){
     return -1;
   }
 
@@ -172,21 +179,26 @@ function parseIdTarefa(param)
 }
 
 app.get("/tarefas", async (req, res) => {
-  try
-  {
+
+  // async - indica que esta rota vai usar operações com a base de dados (tem de aguardar pela receção dos dados)
+
+  try {// try - executa o código normalmente se ocorrer erro o programa não crasha
+    
     const tarefas = await prisma.tarefa.findMany();
+
     return res.status(200).json(tarefas);
   }
-  catch (erro)
-  {
+  catch (erro) {
+    // o catch  trata do erro se for no try
+    // mostra o erro no servidor e devolver resposta desse erro
+
     console.error("ERRO GET /tarefas:", erro);
     return res.status(500).json({ erro: "Erro ao listar tarefas." });
   }
 });
 
 app.get("/tarefas/statistics", async (req, res) => {
-  try
-  {
+  try{
     const tarefas = await prisma.tarefa.findMany();
 
     const total = tarefas.length;
@@ -204,38 +216,36 @@ app.get("/tarefas/statistics", async (req, res) => {
 
     return res.status(200).json({ total, completas, pendentes });
   }
-  catch (erro)
-  {
+  catch (erro){
     console.error("ERRO GET /tarefas/statistics:", erro);
     return res.status(500).json({ erro: "Erro ao calcular estatísticas." });
   }
 });
 
 app.get("/tarefas/:id", async (req, res) => {
-  try
-  {
+  try{
     const id_tarefa = parseIdTarefa(req.params.id);
 
-    if (id_tarefa === -1)
-    {
+    if (id_tarefa === -1){
       return res.status(400).json({ erro: "Id inválido" });
     }
 
-    const tarefa = await prisma.tarefa.findUnique({
-      where: {
+    
+    const tarefa = await prisma.tarefa.findUnique({ // findUnique - procura só um registo
+
+      
+      where: { // where - define qual é a condição da procura (neste caso: id)
         id: id_tarefa
       }
     });
 
-    if (!tarefa)
-    {
+    if (!tarefa){
       return res.status(404).json({ erro: "Tarefa não encontrada" });
     }
 
     return res.status(200).json(tarefa);
   }
-  catch (erro)
-  {
+  catch (erro){
     console.error("ERRO GET /tarefas/:id:", erro);
     return res.status(500).json({ erro: "Erro ao procurar tarefa." });
   }
@@ -247,14 +257,12 @@ app.post("/tarefas", async (req, res) => {
     const titulo = req.body.titulo;
     const prioridade = req.body.prioridade;
 
-    if (!titulo || !prioridade)
-    {
-      return res.status(400).json({ erro: "A indicação de título e prioridade são obrigatórios" });
+    if (!titulo || !prioridade){
+      return res.status(400).json({ erro: "A indicação do título e da prioridade são obrigatórias" });
     }
 
-    if (!prioridadeValida(prioridade))
-    {
-      return res.status(400).json({ erro: "Prioridade inválida. Nota: Só existe 3 tipos de prioridade: baixa, média ou alta" });
+    if (!prioridadeValida(prioridade)){
+      return res.status(400).json({ erro: "Prioridade inválida. Nota: Só existem 3 tipos de prioridade: baixa, média ou alta" });
     }
 
     const novaTarefa = await prisma.tarefa.create({
@@ -267,20 +275,17 @@ app.post("/tarefas", async (req, res) => {
 
     return res.status(201).json(novaTarefa);
   }
-  catch (erro)
-  {
+  catch (erro){
     console.error("ERRO POST /tarefas:", erro);
     return res.status(500).json({ erro: "Erro ao criar tarefa." });
   }
 });
 
 app.put("/tarefas/:id", async (req, res) => {
-  try
-  {
+  try{
     const id_tarefa = parseIdTarefa(req.params.id);
 
-    if (id_tarefa === -1)
-    {
+    if (id_tarefa === -1){
       return res.status(400).json({ erro: "Id inválido" });
     }
 
@@ -290,8 +295,7 @@ app.put("/tarefas/:id", async (req, res) => {
       }
     });
 
-    if (!tarefa)
-    {
+    if (!tarefa){
       return res.status(404).json({ erro: "Tarefa não encontrada" });
     }
 
@@ -301,18 +305,15 @@ app.put("/tarefas/:id", async (req, res) => {
 
     let dadosAtualizados = {};
 
-    if (titulo !== undefined)
-    {
-      if (!titulo)
-      {
-        return res.status(400).json({ erro: "A secção do titulo não pode ser vazio" });
+    if (titulo !== undefined){
+      if (!titulo){
+        return res.status(400).json({ erro: "A secção do título não pode estar vazia" });
       }
 
       dadosAtualizados.titulo = titulo;
     }
 
-    if (concluida !== undefined)
-    {
+    if (concluida !== undefined){
       if (concluida !== true && concluida !== false)
       {
         return res.status(400).json({ erro: "Para estar concluída temos de colocar true ou false" });
@@ -321,39 +322,38 @@ app.put("/tarefas/:id", async (req, res) => {
       dadosAtualizados.concluida = concluida;
     }
 
-    if (prioridade !== undefined)
-    {
-      if (!prioridadeValida(prioridade))
-      {
-        return res.status(400).json({ erro: "Prioridade inválida. Nota: Só existe 3 tipos de prioridade: baixa, média ou alta" });
+    if (prioridade !== undefined){
+      if (!prioridadeValida(prioridade)){
+        return res.status(400).json({ erro: "Prioridade inválida. Nota: Só existem 3 tipos de prioridade: baixa, média ou alta" });
       }
 
       dadosAtualizados.prioridade = prioridade;
     }
 
     const tarefaAtualizada = await prisma.tarefa.update({
+
+      // update  - atualiza os dados do registo
+
       where: {
         id: id_tarefa
       },
+
       data: dadosAtualizados
     });
 
     return res.status(200).json(tarefaAtualizada);
   }
-  catch (erro)
-  {
+  catch (erro){
     console.error("ERRO PUT /tarefas/:id:", erro);
     return res.status(500).json({ erro: "Erro ao atualizar tarefa." });
   }
 });
 
 app.delete("/tarefas/:id", async (req, res) => {
-  try
-  {
+  try{
     const id_tarefa = parseIdTarefa(req.params.id);
 
-    if (id_tarefa === -1)
-    {
+    if (id_tarefa === -1){
       return res.status(400).json({ erro: "Id inválido" });
     }
 
@@ -363,8 +363,7 @@ app.delete("/tarefas/:id", async (req, res) => {
       }
     });
 
-    if (!tarefa)
-    {
+    if (!tarefa){
       return res.status(404).json({ erro: "Tarefa não encontrada" });
     }
 
@@ -376,15 +375,13 @@ app.delete("/tarefas/:id", async (req, res) => {
 
     return res.status(200).json({ mensagem: "Tarefa removida" });
   }
-  catch (erro)
-  {
+  catch (erro){
     console.error("ERRO DELETE /tarefas/:id:", erro);
     return res.status(500).json({ erro: "Erro ao remover tarefa." });
   }
 });
 
-if (process.env.NODE_ENV !== "production")
-{
+if (process.env.NODE_ENV !== "production"){
   app.listen(PORT, () => {
     console.log(`Servidor a correr em http://localhost:${PORT}`);
   });
